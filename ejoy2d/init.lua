8a8576edf893d9d9a513c64d3664cf7bd2338b61
危自林 <wzl@ejoy.com>
2014-03-10 15:23:39 +0800

    register EJOY_MESSAGE for message

--- ejoy2d/init.lua
+++ ejoy2d/init.lua
@@ -24,13 +24,15 @@ local gesture = {
 function ejoy2d.start(callback)
 	fw.EJOY2D_UPDATE = assert(callback.update)
 	fw.EJOY2D_DRAWFRAME = assert(callback.drawframe)
+
 	fw.EJOY2D_TOUCH = function(x,y,what,id)
 		return callback.touch(touch[what],x,y,id)
 	end
     fw.EJOY2D_GESTURE = function(what, x1, y1, x2, y2, state)
 		return callback.gesture(gesture[what], x1, y1, x2, y2, state)
 	end
-  fw.EJOY2D_HANDLE_ERROR = assert(callback.handle_error)
+	fw.EJOY2D_MESSAGE = assert(callback.message)
+  	fw.EJOY2D_HANDLE_ERROR = assert(callback.handle_error)
 	fw.inject()
 end
 
