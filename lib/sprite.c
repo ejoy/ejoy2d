54ea95b82af248b07e6d92224a8118e19a020251
sara_mio <sara_mio@hotmail.com>
2015-04-07 20:15:02 +0800

    修正文字图片在draw_scene里画不出来的bug

--- lib/sprite.c
+++ lib/sprite.c
@@ -629,11 +629,7 @@ sprite_draw_child(struct sprite *s, struct srt *srt, struct sprite_trans * ts, s
 		if (s->data.rich_text && global_lable_only != 1) {
 			t->program = PROGRAM_DEFAULT;	// label never set user defined program
 			switch_program(t, s->s.label->edge ? PROGRAM_TEXT_EDGE : PROGRAM_TEXT, material);
-            if (draw_scene) {
-                label_draw(s->data.rich_text, s->s.label, &viewport_srt, t);
-            } else {
-                label_draw(s->data.rich_text, s->s.label, srt, t);
-            }
+            label_draw(s->data.rich_text, s->s.label, draw_scene ? &viewport_srt : srt, t);
 
             if (s->data.rich_text->sprite_count > 0) {
                 int pre_value = global_lable_only;
@@ -642,7 +638,13 @@ sprite_draw_child(struct sprite *s, struct srt *srt, struct sprite_trans * ts, s
                 switch_program(t, PROGRAM_PICTURE, material);
                 if (s->data.rich_text->label_color_enable)
                     t->color = label_get_color(s->s.label, t);
-                label_draw_sprite(s->data.rich_text, srt, t);
+                if (draw_scene) {
+                    draw_scene = false;
+                    label_draw_sprite(s->data.rich_text, &viewport_srt, t);
+                    draw_scene = true;
+                } else {
+                    label_draw_sprite(s->data.rich_text, srt, t);
+                }
                 
                 global_lable_only = pre_value;
             }
