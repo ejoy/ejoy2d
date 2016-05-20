faf1b0c74eaabf1d433e81a69ddcd35dc0f5c12b
raine <raine_l@163.com>
2016-05-20 15:56:46 +0800

    去掉debug log

--- lib/spritepack.c
+++ lib/spritepack.c
@@ -431,7 +431,7 @@ limport(lua_State *L) {
 	alloc.buffer = (char *)lua_newuserdata(L, size);
 	alloc.cap = size;
     
-    printf("addr %p size%d\n", alloc.buffer, alloc.cap);
+//    printf("addr %p size%d\n", alloc.buffer, alloc.cap);
 
 	struct sprite_pack *pack = (struct sprite_pack *)ialloc(&alloc, SIZEOF_PACK + tex * sizeof(int));
 	pack->n = max_id + 1;
